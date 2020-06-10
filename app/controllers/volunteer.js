eb5a46ac77223b593de7c08758d2644134bffb77
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-06-10 10:07:22 +0530

    volunteer sign up and login

--- app/controllers/volunteer.js
+++ app/controllers/volunteer.js
@@ -1,15 +1,79 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const Volunteer = require('../models/volunteer');
+const jwt = require('jsonwebtoken');
+const jwt_key = "mykey";
 
 exports.login = (req, res, next) => {
-    res.status(200).json({
-        message: "handling volunteer login"
-    });
+    Volunteer.find({ Volunteer_email: req.body.Volunteer_email })
+        .exec()
+        .then(doc => {
+            if (doc.lenght < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.Volunteer_password, doc[0].Volunteer_password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                            email: doc[0].Volunteer_email,
+                            userId: doc[0]._id
+                        },
+                        jwt_key, {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: "Auth successfull",
+                        token: token
+                    });
+                }
+            });
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 }
 
 exports.signUp = (req, res, next) => {
-    res.status(200).json({
-        message: "handling volunteer signUp"
+    bcrypt.hash(req.body.Volunteer_password, 10, (err, result) => {
+        if (err) {
+            return res.status(401).json({
+                message: "Auth failed"
+            })
+        }
+        if (result) {
+            const volunteer = new Volunteer({
+                Volunteer_id: new mongoose.Types.ObjectId(),
+                Volunteer_name: req.body.Volunteer_name,
+                Volunteer_email: req.body.Volunteer_email,
+                Volunteer_password: result,
+                Volunteer_phone: req.body.Volunteer_phone,
+                Volunteer_class: req.body.Volunteer_class,
+                Volunteer_divison: req.body.Volunteer_divison
+            });
+            volunteer.save()
+                .then(doc => {
+                    console.log(doc);
+                    res.status(200).json({
+                        message: "succesfully registered",
+                        result: doc
+                    });
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        }
     });
 }
 
--- app/models/volunteer.js
+++ app/models/volunteer.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const VolunteerSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    Volunteer_id: mongoose.Schema.Types.ObjectId,
     Volunteer_name: String,
     Volunteer_email: String,
     Volunteer_password: String,
     Volunteer_phone: Number,
-    Volunteer_class: Number,
-    Volunteer_divison: String,
-    required: true
+    Volunteer_class: String,
+    Volunteer_divison: String
 });
 
 module.exports = mongoose.model('Volunteers', VolunteerSchema);
