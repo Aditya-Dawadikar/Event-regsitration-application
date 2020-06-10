34c4495f433fc78ac1de99346a9d8a6a7b47f149
Aditya Dawadikar <adityadawadikar2000@gmail.com>
2020-06-10 09:31:38 +0530

    Admin countrollers signup and login

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -1,22 +1,98 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const Admin = require('../models/admin');
+const jwt = require('jsonwebtoken');
+const jwt_key = "mykey";
 
 exports.login = (req, res, next) => {
-    res.status(200).json({
-        message: "handling admin login"
-    });
+    Admin.find({ Admin_email: req.body.Admin_email })
+        .exec()
+        .then(doc => {
+            if (doc.length < 1) {
+                return res.status(401).json({
+                    message: "Auth failed"
+                });
+            }
+            bcrypt.compare(req.body.Admin_password, doc[0].Admin_password, (err, result) => {
+                if (err) {
+                    res.status(401).json({
+                        message: "Auth failed"
+                    });
+                }
+                if (result) {
+                    const token = jwt.sign({
+                            email: doc[0].Admin_email,
+                            userId: doc[0]._id
+                        },
+                        jwt_key, {
+                            expiresIn: "1h"
+                        }
+                    );
+                    return res.status(200).json({
+                        message: "Auth successful",
+                        token: token
+                    });
+                }
+                res.status(401).json({
+                    message: "Auth Failed"
+                });
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
 }
 
 exports.signUp = (req, res, next) => {
-    res.status(200).json({
-        message: "handling admin signup"
+    if (!req.body) {
+        return res.status(400).json({
+            message: "req body cannot be empty"
+        });
+    }
+    bcrypt.hash(req.body.Admin_password, 10, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+            return
+        } else {
+            const admin_password = result;
+            const admin = new Admin({
+                Admin_id: new mongoose.Types.ObjectId(),
+                Admin_name: req.body.Admin_name,
+                Admin_email: req.body.Admin_email,
+                Admin_password: admin_password
+            });
+
+            admin.save()
+                .then(doc => {
+                    console.log(doc);
+                    res.status(200).json(doc);
+                })
+                .catch(err => {
+                    res.status(500).json({
+                        error: err
+                    });
+                });
+        }
     });
 }
 
 exports.getAll = (req, res, next) => {
-    res.status(200).json({
-        message: "handling getAll"
-    });
+    Admin.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json(docs);
+            console.log(json);
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
 }
 
 exports.patchById = (req, res, next) => {
--- app/models/admin.js
+++ app/models/admin.js
@@ -3,8 +3,7 @@ const AdminSchema = mongoose.Schema({
     Admin_id: mongoose.Schema.Types.ObjectId,
     Admin_name: String,
     Admin_email: String,
-    Admin_password: String,
-    required: true
+    Admin_password: String
 });
 
 module.exports = mongoose.model('Admins', AdminSchema);
